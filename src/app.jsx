ecd8d26372199fb5d36c36ef952c1592be078b51
Spencer Judd <spencercjudd@gmail.com>
2015-06-09 16:23:37 -0400

    Basic Flux architecture.
    This is the typical singleton-dispatcher-with-a-switch style. Will
    probably want to move to something more robust later.

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,44 @@
 import React, { Component } from 'react';
+import { Dispatcher } from 'flux';
+import MicroEvent from 'microevent';
 import io from 'socket.io-client';
+
+function uuid() {
+  var i, random;
+  var uuid = '';
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
+      uuid += '-';
+    }
+
+    uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+  }
+  return uuid;
+}
+
+var MessageStore = function() {
+  this.items = [];
+}
+MessageStore.prototype.getAll = function() {
+  return this.items;
+}
+MicroEvent.mixin(MessageStore);
+
+var messageStore = new MessageStore();
+
+var AppDispatcher = new Dispatcher();
+
+AppDispatcher.register(function(payload) {
+  switch(payload.eventName) {
+    case 'new-message':
+      messageStore.items.push(payload.message);
+      messageStore.trigger('change');
+      break;
+  }
+  return true;
+});
+
 
 function zeroPad(num) {
   return num < 10 ? '0' + num : num;
@@ -54,29 +93,44 @@ class DateSeparator extends Component {
 }
 
 class Messages extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: [
-        <DateSeparator key="sep-1" date={new Date("01/01/2015")} />,
-        <Message key="msg-1" date={new Date("01/01/2015 12:36:00")} nick="n0c" body="test message 1" />,
-        <DateSeparator key="sep-2" date={new Date("01/02/2015")} />,
-        <Message key="msg-2" date={new Date("01/02/2015 12:37:00")} nick="Shadow_Wanderer" body="test message 2" />,
-        <Message key="msg-3" date={new Date("01/02/2015 12:38:00")} nick="n0c" body="test message 3" />
-      ]
-    };
+  constructor() {
+    super();
+    this.state = {messages: []};
+    this.changed = this.changed.bind(this);
+    this.render = this.render.bind(this);
   }
+  componentDidMount()    { messageStore.bind(  'change', this.changed); }
+  componentWillUnmount() { messageStore.unbind('change', this.changed); }
+  changed()              { this.setState({messages: messageStore.getAll()}); }
 
   render() {
+    var lastDate;
+    var messages = this.state.messages.map(function(msg) {
+      return <Message key={uuid()} date={msg.date} nick={msg.nick} body={msg.body} />
+    });
     return (
       <div className="messages">
-        {this.state.messages}
+        {messages}
       </div>
     );
   }
 }
 
 class MessageBar extends Component {
+  update(e) {
+    if (e.which == 13) {
+      AppDispatcher.dispatch({
+        eventName: 'new-message',
+        message: {
+          nick: 'n0c',
+          date: new Date(),
+          body: e.target.value
+        }
+      });
+      e.target.value = "";
+    }
+  }
+
   render() {
     return (
       <div
@@ -89,6 +143,7 @@ class MessageBar extends Component {
         }}>
         <input
           type="text"
+          onKeyUp={this.update}
           className="messageBar"
           placeholder="Type your message here"
           style={{
@@ -105,9 +160,11 @@ class MessageBar extends Component {
 }
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  componentWillMount() {
     this.socket = io();
+    this.socket.on('connect', function() {
+      console.log('connected!');
+    });
   }
 
   render() {
