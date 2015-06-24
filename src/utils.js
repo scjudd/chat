08c6f13c93a2ea0243afe8cfd6a1492291147f85
Spencer Judd <spencercjudd@gmail.com>
2015-06-24 09:35:33 -0400

    Removed unused client-side UUID function.

--- src/utils.js
+++ src/utils.js
@@ -10,20 +10,6 @@ export function m(...args) {
   return res;
 }
 
-export function uuid() {
-  let i, random;
-  let uuid = '';
-  for (i = 0; i < 32; i++) {
-    random = Math.random() * 16 | 0;
-    if (i === 8 || i === 12 || i === 16 || i === 20) {
-      uuid += '-';
-    }
-
-    uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-  }
-  return uuid;
-}
-
 function zeroPad(num) {
   return num < 10 ? '0' + num : num;
 }
