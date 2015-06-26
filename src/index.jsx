5a95196dece216ebe8d33c556cbf768c8e48bad5
Spencer Judd <spencercjudd@gmail.com>
2015-06-26 16:00:19 -0400

    Added unread message count to <title>.
    This has the side-effect of causing the tab to pulsate in certain
    browsers. For example, in Chrome, if the tab is pinned and not 'active',
    the tab will pulsate when a new message comes in.

--- src/index.jsx
+++ src/index.jsx
@@ -2,3 +2,27 @@ import React from 'react';
 import App from './components/App';
 
 React.render(<App />, document.getElementById('root'));
+
+import messageStore from './stores/messages';
+
+let lastMsgCount;
+let isActive = true;
+let original = document.title;
+
+messageStore.listen(function(messages) {
+  lastMsgCount = lastMsgCount || messages.length - 1;
+
+  if (!isActive) {
+    document.title = '(' + (messages.length - lastMsgCount) + ') ' + original;
+  }
+});
+
+window.addEventListener('focus', function() {
+  lastMsgCount = undefined;
+  isActive = true;
+  document.title = original;
+});
+
+window.addEventListener('blur', function() {
+  isActive = false;
+});
