e1c7125b7d337752d22b83ac512f338bb172843c
Spencer Judd <spencercjudd@gmail.com>
2015-06-26 16:29:14 -0400

    Only count *actual* messages in the new message total.

--- src/index.jsx
+++ src/index.jsx
@@ -5,20 +5,22 @@ React.render(<App />, document.getElementById('root'));
 
 import messageStore from './stores/messages';
 
-let lastMsgCount;
+let newMessages = 0;
 let isActive = true;
 let original = document.title;
 
 messageStore.listen(function(messages) {
-  lastMsgCount = lastMsgCount || messages.length - 1;
+  if (messages[messages.length-1].type === 'message') {
+    newMessages += 1;
+  }
 
-  if (!isActive) {
-    document.title = '(' + (messages.length - lastMsgCount) + ') ' + original;
+  if (!isActive && newMessages > 0) {
+    document.title = '(' + newMessages + ') ' + original;
   }
 });
 
 window.addEventListener('focus', function() {
-  lastMsgCount = undefined;
+  newMessages = 0;
   isActive = true;
   document.title = original;
 });
